a3bc52a7db383d5844a920290ad3e2b1b33221f6
unknown <uday kiran>
2023-03-11 19:20:34 +0530

    Initial commit

---
+++ main_project.js
@@ -0,0 +1,36 @@
+searchform=document.querySelector('search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+}
+
+let loginform = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginform.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+    loginform.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+    searchform.classList.remove('active');
+    
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+
+
+
+window.onload = ()=>{
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
